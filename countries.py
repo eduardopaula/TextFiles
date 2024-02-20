416b56f074cbce3490f33de33f4c3c9e49978f62
Eduardo de Paula Neves <eduardo.paula@gmail.com>
2024-02-20 14:02:09 -0300

    Aula 252

--- countries.py
+++ countries.py
@@ -3,4 +3,5 @@ input_filename = 'country_info.txt'
 with open(input_filename) as country_file:
     for row in country_file:
         data = row.strip('\n').split('|')
-        print(data)
+        country, capital, code, code3, dialing, timezone, currency = data
+        print(country, capital, code, code3, dialing, timezone, currency , sep='\n\t')
