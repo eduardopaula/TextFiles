d7f5663570a7e81e88a75e6e9cc54539782eb348
Eduardo de Paula Neves <eduardo.paula@gmail.com>
2024-02-22 11:18:44 -0300

    Aula 264

--- global_temps.py
+++ global_temps.py
@@ -1,9 +1,14 @@
 import json
+import urllib.request
 
-json_data_source = 'temperature_anomaly.json'
+# json_data_source = 'temperature_anomaly.json'
+json_data_source = 'https://www.ncei.noaa.gov/access/monitoring/climate-at-a-glance/global/time-series/globe/land_ocean/1/10/1880-2022.json'
 
-with open(json_data_source, encoding='utf-8') as data:
-    anomalies = json.load(data)
+# with open(json_data_source, encoding='utf-8') as data:
+with urllib.request.urlopen(json_data_source) as json_stream:
+    data = json_stream.read().decode('utf-8')
+    anomalies = json.loads(data)
+
 
 print(anomalies['description'])
 
