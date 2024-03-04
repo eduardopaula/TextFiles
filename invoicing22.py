14ccdfecb20f89f403df738af16b759263c8b725
Eduardo de Paula Neves <eduardo.paula@gmail.com>
2024-03-04 13:30:56 -0300

    Aula 281

--- invoicing22.py
+++ invoicing22.py
@@ -42,48 +42,38 @@ def next_invoice_number(invoice_number: str) -> str:
     else:
         invoice_year = year
         number = 1
-    # return f'{invoice_year}-{number:04d}'
     new_invoice_number = f'{invoice_year}-{number:04d}'
     return new_invoice_number
 
+
 def record_invoice(invoice_file: TextIO,
                    company: str,
-                   amount: float) -> int:
+                   amount: float) -> None:
     """Create a new invoice number, and write it to a file on disk.
 
     :param invoice_file: An open text file, opened using r+.
     :param company: The name of the company being invoiced.
     :param amount: The amount of the invoice.
-    :param last_line_ptr: The position of the start of the last
-        line in the file. This will be obtained by the previous
-        call to `record_invoice`.
-    :return: The position of the start of the last line in the file.
-        This can be used in subsequent calls to `record_invoice`.
     """
-    last_line_ptr = 0
-    invoice_file.seek(last_line_ptr, SEEK_SET)
     last_row = ''
     for line in invoice_file:
-        print('*', end='') #  TODO delete ater testing
+        print('*', end='')  # TODO delete after testing
         last_row = line
     if last_row:
-        # invoice_number, a, c = last_row.split('\t')
         # invoice_number, _, _ = last_row.split('\t')
         invoice_number = last_row.split('\t')[0]
         new_invoice_number = next_invoice_number(invoice_number)
     else:
         # if the file is empty, we'll start numbering from 1.
         year = get_year()
-        new_invoice_number = f'{year}={1:04d}'
+        new_invoice_number = f'{year}-{1:04d}'
 
-    last_line_ptr = invoice_file.tell()
     print(f'{new_invoice_number}\t{company}\t{amount}', file=invoice_file)
-    return last_line_ptr
 
 
 data_file = 'invoices.csv'
 
-with open(data_file, 'r+', encoding='utf-8') as invoices:
+with open(data_file, 'r+') as invoices:
     record_invoice(invoices, 'ACME Roadrunner', 18.40)
     record_invoice(invoices, 'Squirrel Storage', 320.55)
 
@@ -98,9 +88,6 @@ with open(data_file, 'r+', encoding='utf-8') as invoices:
 #
 # for test_string, result, next_number in test_data:
 #     parts = parse_invoice_number(test_string)
-#     print('test_string: ',test_string)
-#     print('result: ', result)
-#     print('next_number: ', next_number)
 #     if parts == result:
 #         print(f'{test_string} parsed successfully')
 #     else:
